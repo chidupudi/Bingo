0bfe2a321f5078ce476657ba15ccded936b32b01
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-08-27 09:15:45 +0530

    changes

--- script.js
+++ script.js
@@ -61,7 +61,6 @@ function handleCellClick(row, col, board) {
 
 
 
-
 function handlePlayerChange() {
     currentPlayer = currentPlayer === "Player" ? "AI" : "Player";
     statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`;
